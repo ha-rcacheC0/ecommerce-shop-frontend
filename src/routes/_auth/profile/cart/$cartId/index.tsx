f20f8420f7f8c6540047678b38caaa8511b79890
Eric Donnelly <200433532@student.georgianc.on.ca>
2024-06-11 19:01:11 -0400

    taking away missed console logs

--- src/routes/_auth/profile/cart/$cartId/index.tsx
+++ src/routes/_auth/profile/cart/$cartId/index.tsx
@@ -13,19 +13,10 @@ export const Route = createFileRoute("/_auth/profile/cart/$cartId/")({
 function CartPage() {
   const { cartId } = Route.useParams();
   const products = useQuery(cartItemsQueryOptions(cartId));
-  console.log(products.data?.products);
 
-  const subtotal = 10.99;
-  const shipping = 5.99;
-  const grandTotal = subtotal + shipping;
   return (
     <div className="p-8 w-3/5 mx-auto">
-      <Cart
-        products={products.data!.products}
-        subtotal={subtotal}
-        shipping={shipping}
-        grandTotal={grandTotal}
-      />
+      <Cart products={products.data!.products} />
     </div>
   );
 }
