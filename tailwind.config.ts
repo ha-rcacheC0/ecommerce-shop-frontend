6db24b70eb99a38c34c9d386cd35841ddd1101d5
Eric Donnelly <eric@donnellydev.ca>
2025-01-03 16:48:35 -0500

    fixed daisy stuff kinda added vercel,json to help with refreshes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,18 +16,47 @@ export default {
     themes: [
       {
         dark: {
-          ...require("daisyui/src/theming/themes")["dark"],
+          "color-scheme": "dark",
           primary: "#002868",
           secondary: "#BF0A30",
+          accent: "oklch(74.51% 0.167 183.61)",
+          neutral: "#2a323c",
+          "neutral-content": "#A6ADBB",
+          "base-100": "#1d232a",
+          "base-200": "#191e24",
+          "base-300": "#15191e",
+          "base-content": "#A6ADBB",
         },
       },
+
+      //   dark: {
+      //     ...require("daisyui/src/theming/themes")["dark"],
+      //     primary: "#002868",
+      //     secondary: "#BF0A30",
+      //   },
+      // },
+      // {
+      //   light: {
+      //     ...require("daisyui/src/theming/themes")["light"],
+      //     primary: "#002868",
+      //     secondary: "#BF0A30",
       {
         light: {
-          ...require("daisyui/src/theming/themes")["light"],
+          "color-scheme": "light",
           primary: "#002868",
           secondary: "#BF0A30",
+          "secondary-content": "oklch(98.71% 0.0106 342.55)",
+          accent: "oklch(76.76% 0.184 183.61)",
+          neutral: "#2B3440",
+          "neutral-content": "#D7DDE4",
+          "base-100": "oklch(100% 0 0)",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E6E6",
+          "base-content": "#1f2937",
         },
       },
+      //   },
+      // },
       "coffee",
       "sunset",
       "emerald",
