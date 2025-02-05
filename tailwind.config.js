746c83a950f29229d0a4ffcc37a086ff9351fa99
Lucas Keller <148141760+ha-rcacheC0@users.noreply.github.com>
2025-02-05 12:58:39 -0600

    removing daisy to update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 
-import daisyui from "daisyui";
 export default {
   content: [
     "./index.html",
@@ -11,33 +10,4 @@ export default {
   theme: {
     extend: {},
   },
-  daisyui: {
-    themeRoot: ":root",
-    themes: [
-      {
-        dark: {
-          ...require("daisyui/src/theming/themes")["dark"],
-          primary: "#002868",
-          secondary: "#BF0A30",
-        },
-      },
-      {
-        light: {
-          ...require("daisyui/src/theming/themes")["light"],
-          primary: "#002868",
-          secondary: "#BF0A30",
-        },
-      },
-      "coffee",
-      "sunset",
-      "emerald",
-    ],
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-  },
-  plugins: [daisyui],
 };
